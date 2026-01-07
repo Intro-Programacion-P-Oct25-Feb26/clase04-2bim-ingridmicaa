280ea8506faf76040baeed667db254a3e7d6f6c1
ingridmicaa <ingridmicaela03@gmail.com>
2026-01-07 12:21:35 -0500

    Ejercicio043

---
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo043.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos02;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo043 {
+
+    public static void main(String[] args) {
+        // 
+        int[] valores1 = {10, 100, 1000, 20, 200, 2000};
+        int[] valores2 = {1000, 1001, 1002, 1002, 1003, 1004, 1005};
+
+        for (int i = 0; i < valores1.length; i++) {
+
+            int v1 = valores1[i];
+            int v2 = valores2[i];
+            obtenerSuma(v1, v2);
+
+        }
+        /*
+        En los valores en los arreglos valores1 y valores2, utilizamos un For 
+        para que recorra todo el arreglo de valores1, despues creamos variales
+        toma un número de valores1 y el número en la misma posición de valores2.
+        Llama al método obtenerSuma para sumar esos dos números.
+         */
+
+    }
+
+    public static void obtenerSuma(int a, int b) {
+        int suma;
+        suma = a + b;
+        System.out.printf("El valor de la suma es: %d\n", suma);
+
+    }
+
+}
